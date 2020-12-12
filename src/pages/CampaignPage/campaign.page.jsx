b1ceb03fa0a264b37c8b32bad87f44cee2d7ca54
pnshiralkar <pnshiralkar@gmail.com>
2020-12-12 20:24:29 +0530

    Campaign Page init

---
+++ src/pages/CampaignPage/campaign.page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Col, Grid, Row} from "antd";
+
+export default function CampaignPage(props){
+    return <Row>
+        <Col span={12}>
+
+        </Col>
+
+        <Col span={12}>
+
+        </Col>
+    </Row>
+}
