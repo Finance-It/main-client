5cf2a826e4c2376f78356bda83011e31272013ef
pratikdaigavane <daigavanep@gmail.com>
2020-12-12 22:07:43 +0530

    register page added

--- src/App.jsx
+++ src/App.jsx
@@ -4,50 +4,52 @@ import {Route, Switch} from 'react-router-dom';
 import {GithubOutlined} from '@ant-design/icons';
 import HomePage from "./pages/HomePage/HomePage.page";
 import Login from "./pages/Login/Login.page"
+import SignUp from "./pages/SignUp/SignUp.page"
 
 const defaultFooterDom = (
     <DefaultFooter
         copyright={`${new Date().getFullYear()} Finance It`}
         links={[
-          {
-            key: 'Website',
-            title: 'Website',
-            href: 'https://pict.acm.org',
-            blankTarget: true,
-          },
-          {
-            key: 'github',
-            title: <GithubOutlined/>,
-            href: 'https://github.com/PICT-ACM-Student-Chapter',
-            blankTarget: true,
-          },
-          {
-            key: 'LinkedIn',
-            title: 'LinkedIn',
-            href: 'https://ant.design',
-            blankTarget: true,
-          },
+            {
+                key: 'Website',
+                title: 'Website',
+                href: 'https://pict.acm.org',
+                blankTarget: true,
+            },
+            {
+                key: 'github',
+                title: <GithubOutlined/>,
+                href: 'https://github.com/PICT-ACM-Student-Chapter',
+                blankTarget: true,
+            },
+            {
+                key: 'LinkedIn',
+                title: 'LinkedIn',
+                href: 'https://ant.design',
+                blankTarget: true,
+            },
         ]}
     />
 );
 
 function App() {
-  return (
+    return (
 
-      <ProLayout
-          title="Finance It"
-          logo="https://pict.acm.org/radiance/img/PASC-W2.png"
-          layout="top"
-          fixedHeader="true"
-          footerRender={() => defaultFooterDom}
+        <ProLayout
+            title="Finance It"
+            logo="https://pict.acm.org/radiance/img/PASC-W2.png"
+            layout="top"
+            fixedHeader="true"
+            footerRender={() => defaultFooterDom}
 
-      >
-          <Switch>
-              <Route exact path="/" component={HomePage}/>
-              <Route exact path="/login" component={Login}/>
-          </Switch>
-      </ProLayout>
-  );
+        >
+            <Switch>
+                <Route exact path="/" component={HomePage}/>
+                <Route exact path="/login" component={Login}/>
+                <Route exact path="/signup" component={SignUp}/>
+            </Switch>
+        </ProLayout>
+    );
 }
 
 export default App;
---
+++ src/pages/SignUp/SignUp.page.jsx
@@ -0,0 +1,132 @@
+import React from 'react';
+import '../Login/login.css';
+import {Button, notification, Form, Input, Typography} from 'antd';
+import {LockOutlined, UserOutlined} from '@ant-design/icons';
+import axios from 'axios'
+
+
+const SignUp = (props) => {
+    const [errors, setErrors] = React.useState('')
+    const [status, setStatus] = React.useState('')
+
+    const onFinish = async values => {
+        setStatus('validating')
+        setErrors('')
+
+        if(values.password !== values.confirm_password)
+        {
+            notification['warning']({
+                message: 'Passwords not matched'
+            });
+            setStatus('error')
+
+            return;
+        }
+        try {
+            console.log(values)
+            values.email = values.username
+            const res = await axios.post(`${process.env.REACT_APP_MAIN_SERVER}/auth/users/`, values)
+            setStatus('success')
+            setErrors('')
+            const token = res.data.access
+            sessionStorage.setItem('token', token)
+            props.history.replace('/login')
+            notification['success']({
+                message: 'User Registered',
+                description:
+                    'Please Login Now!',
+            });
+            setStatus('error')
+        } catch (e) {
+            console.log(e.response)
+            let errors = '\n'
+            if(e.response.data.username){
+                errors += e.response.data.username.join("\n")
+            }
+            if(e.response.data.password){
+                errors += e.response.data.password.join("\n")
+            }
+            setStatus('error')
+            setErrors(errors)
+        }
+    };
+
+    return (
+        <div className='login-container'>
+            <Form
+                name="normal_login"
+                className="login-form"
+                initialValues={{
+                    remember: true,
+                }}
+                onFinish={onFinish}
+
+            >
+                <Typography.Title align={'center'}>Sign Up</Typography.Title>
+                <br/><br/>
+                <Form.Item
+                    name="username"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your Email!',
+                        },
+                    ]}
+                    validateStatus={status}
+                    hasFeedback={true}
+                >
+                    <Input type="email"
+                           size='large' prefix={<UserOutlined className="site-form-item-icon"/>} placeholder="Email"
+                           errors={'dd'}/>
+                </Form.Item>
+                <Form.Item
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your Password!',
+                        },
+                    ]}
+                    validateStatus={status}
+                    hasFeedback={true}
+
+                >
+                    <Input
+                        size='large'
+                        prefix={<LockOutlined className="site-form-item-icon"/>}
+                        type="password"
+                        placeholder="Password"
+                    />
+                </Form.Item>
+                <Form.Item
+                    name="confirm_password"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your Password again!',
+                        },
+                    ]}
+                    validateStatus={status}
+                    hasFeedback={true}
+                    help={errors}
+                >
+                    <Input
+                        size='large'
+                        prefix={<LockOutlined className="site-form-item-icon"/>}
+                        type="password"
+                        placeholder="Confirm Password"
+                    />
+                </Form.Item>
+                <Form.Item>
+                    <Button size='large' type="primary" htmlType="submit" className="login-form-button">
+                        Log in
+                    </Button>
+                    <br/><br/>
+                </Form.Item>
+            </Form>
+        </div>
+
+    );
+};
+
+export default SignUp
