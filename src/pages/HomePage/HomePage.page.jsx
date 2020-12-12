364054d4b72e6174dfc24d9d173325832a810c69
AVC0706 <atharvvijay4@gmail.com>
2020-12-12 19:18:43 +0530

    card responsive wrapper

--- src/pages/HomePage/HomePage.page.jsx
+++ src/pages/HomePage/HomePage.page.jsx
@@ -1,6 +1,8 @@
-import {Carousel} from 'antd';
+import {Card, Carousel, Col, Row} from 'antd';
+
 import React from 'react'
 
+const {Meta} = Card;
 const contentStyle = {
     height: '400px',
     color: '#fff',
@@ -10,23 +12,66 @@ const contentStyle = {
 };
 
 
-const HomePage = ()=>{
+const HomePage = () => {
 
-    return(
-        <Carousel autoplay>
-            <div>
-                <h3 style={contentStyle}>1</h3>
-            </div>
-            <div>
-                <h3 style={contentStyle}>2</h3>
-            </div>
-            <div>
-                <h3 style={contentStyle}>3</h3>
-            </div>
-            <div>
-                <h3 style={contentStyle}>4</h3>
-            </div>
-        </Carousel>
+
+
+
+    return (
+        <div>
+
+            <Carousel autoplay>
+                <div>
+                    <h3 style={contentStyle}>1</h3>
+                </div>
+                <div>
+                    <h3 style={contentStyle}>2</h3>
+                </div>
+                <div>
+                    <h3 style={contentStyle}>3</h3>
+                </div>
+                <div>
+                    <h3 style={contentStyle}>4</h3>
+                </div>
+            </Carousel>
+
+
+
+            <Row gutter={[8, 32]} justify="center" style={{ justifyContent:"center", marginLeft:"10%" }}>
+                <Col sm={24} md={8} lg={6}>
+                    <Card
+                        hoverable
+                        style={{width: 240}}
+                        cover={<img alt="example"
+                                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"/>}
+                    >
+                        <Meta title="Europe Street beat" description="www.instagram.com"/>
+                    </Card>
+                </Col>
+                <Col sm={24} md={8} lg={6}>
+                    <Card
+                        hoverable
+                        style={{width: 240}}
+                        cover={<img alt="example"
+                                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"/>}
+                    >
+                        <Meta title="Europe Street beat" description="www.instagram.com"/>
+                    </Card>
+                </Col>
+                <Col sm={12} md={8} lg={6}>
+                    <Card
+                        hoverable
+                        style={{width: 240}}
+                        cover={<img alt="example"
+                                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"/>}
+                    >
+                        <Meta title="Europe Street beat" description="www.instagram.com"/>
+                    </Card>
+                </Col>
+
+            </Row>
+
+        </div>
     )
 }
 
